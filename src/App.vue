db9d4dc7580c77bafa7a4df72ad15b860b126b04
itMdh <itmdh@foxmail.com>
2024-01-23 22:42:01 +0800

    pokeman Go Card 3D

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Demo from "@/demo/demo.vue";
+</script>
+
+<template>
+<div>
+  <demo/>
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/demo/demo.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import ImagePoke from "@/components/Image-Poke.vue";
+import {ref} from "vue";
+const path = ref(new URL('@/assets/img.png', import.meta.url))
+const width = ref('10vw')
+const height = ref('20vh')
+const pop = ref('true')
+</script>
+
+<template>
+  <div>
+    <div>
+      <ImagePoke :path="path"/>
+      单一实例
+    </div>
+    <div>
+    <ImagePoke :path="path" :width="width" :height="height"/>
+      自定义宽高
+    </div>
+    <div>
+      <ImagePoke :path="path" :width="width" :height="height" :pop="pop"/>
+      可点击放大
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
